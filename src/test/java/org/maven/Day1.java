a3da069bb02598593eb1baac7a7907eaedb754f1
Hameed-Hussain <hhameed.hussain111@gmail.com>
2021-11-28 19:45:11 +0530

    redone

--- src/test/java/org/maven/Day1.java
+++ src/test/java/org/maven/Day1.java
@@ -13,5 +13,9 @@ public static void main(String[] args) {
 	
 	driver.get("https://www.amazon.in/");
 	driver.manage().window().maximize();
+	
+	System.out.println("changes");
+	System.out.println("changes");
+	System.out.println("changes");
 }
 }
